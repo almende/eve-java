91a3d6a5ccb4d2ce6930f68b8faa5bbf16cc433a
Ronny Hendrawan <ronny@dealservices.nl>
2014-02-05 15:14:21 +0100

    first implementation of mongo state factory

---
+++ java/eve-core/src/main/java/com/almende/eve/state/mongo/MongoState.java
@@ -0,0 +1,257 @@
+package com.almende.eve.state.mongo;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import org.jongo.Jongo;
+import org.jongo.marshall.jackson.oid.Id;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.almende.eve.rpc.jsonrpc.jackson.JOM;
+import com.almende.eve.state.AbstractState;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.NullNode;
+import com.mongodb.WriteResult;
+
+/**
+ * Representation of Eve agents state based on MongoDB
+ * 
+ * @author ronny
+ *
+ */
+public class MongoState extends AbstractState<JsonNode> {
+	
+	public static final String COLLECTION_NAME = "agents";
+	
+	private static final Logger log = LoggerFactory.getLogger(MongoState.class);
+	
+	private Map<String, JsonNode>	properties	= Collections.synchronizedMap(new HashMap<String, JsonNode>());
+	
+	@JsonIgnore
+	private Jongo connection;
+	
+	/**
+	 * @see com.almende.eve.state.AbstractState#AbstractState()
+	 */
+	public MongoState() {
+	}
+	
+	/**
+	 * main constructor: with agentId as input parameter
+	 * @param agentId
+	 */
+	public MongoState(final String agentId) {
+		super(agentId);
+	}
+	
+	@JsonIgnore
+	public void setConnection(Jongo connection) {
+		this.connection = connection;
+	}
+	
+	@JsonIgnore
+	public Jongo getConnection() {
+		return connection;
+	}
+	
+	/**
+	 * returns agent ID and adding @Id annotation to mark it as objectID in Mongo
+	 * otherwise mongo will generate new object each time
+	 * 
+	 * @see com.almende.eve.state.State#getAgentId()
+	 */
+	@Override
+	@Id
+	public synchronized String getAgentId() {
+		return super.getAgentId();
+	}
+
+	/* (non-Javadoc)
+	 * @see com.almende.eve.state.State#init()
+	 */
+	@Override
+	public void init() {
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.almende.eve.state.State#destroy()
+	 */
+	@Override
+	public void destroy() {
+	}
+
+	/* (non-Javadoc)
+	 * @see com.almende.eve.state.State#remove(java.lang.String)
+	 */
+	@Override
+	public synchronized Object remove(final String key) {
+		Object result = null;
+		try {
+			result = properties.remove(key);
+			updateObject();
+		} catch (final Exception e) {
+			log.warn("remove error : "+e.getMessage());
+		}
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.almende.eve.state.State#containsKey(java.lang.String)
+	 */
+	@Override
+	public boolean containsKey(final String key) {
+		boolean result = false;
+		try {
+			result = properties.containsKey(key);
+		} catch (final Exception e) {
+			log.warn("containsKey error : "+e.getMessage());
+		}
+		return result;
+	}
+
+	@Override
+	public Set<String> keySet() {
+		Set<String> result = null;
+		try {
+			result = properties.keySet();
+		} catch (final Exception e) {
+			log.warn("keySet error : "+e.getMessage());
+		}
+		return result;
+	}
+
+	@Override
+	public void clear() {
+		try {
+			final String agentType = properties.get(KEY_AGENT_TYPE).textValue();
+			properties.clear();
+			properties.put(KEY_AGENT_TYPE, JOM.getInstance().valueToTree(agentType));
+			updateObject();
+		} catch (final Exception e) {
+			log.warn("clear error : "+e.getMessage());
+		}	
+	}
+
+
+	@Override
+	public int size() {
+		int result = 0;
+		try {
+			result = properties.size();
+		} catch (final Exception e) {
+			log.warn("size error : "+e.getMessage());
+		}
+		return result;
+	}
+
+	@Override
+	public JsonNode get(String key) {
+		JsonNode result = null;
+		try {
+			result = properties.get(key);
+		} catch (final Exception e) {
+			log.warn("get error : "+e.getMessage());
+		}
+		return result;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.almende.eve.state.AbstractState#locPut(java.lang.String, com.fasterxml.jackson.databind.JsonNode)
+	 */
+	@Override
+	public synchronized JsonNode locPut(final String key, final JsonNode value) {
+		JsonNode result = null;
+		try {
+			result = properties.put(key, value);
+			updateObject();
+		} catch (final Exception e) {
+			log.warn("locPut error : "+e.getMessage());
+		}
+		
+		return result;
+	}
+	
+	/* (non-Javadoc)
+	 * @see com.almende.eve.state.AbstractState#locPutIfUnchanged(java.lang.String, com.fasterxml.jackson.databind.JsonNode, com.fasterxml.jackson.databind.JsonNode)
+	 */
+	@Override
+	public synchronized boolean locPutIfUnchanged(final String key,
+			final JsonNode newVal, JsonNode oldVal) {
+		boolean result = false;
+		try {
+			JsonNode cur = NullNode.getInstance();
+			if (properties.containsKey(key)) {
+				cur = properties.get(key);
+			}
+			if (oldVal == null) {
+				oldVal = NullNode.getInstance();
+			}
+			
+			// Poor man's equality as some Numbers are compared incorrectly: e.g.
+			// IntNode versus LongNode
+			if (oldVal.equals(cur) || oldVal.toString().equals(cur.toString())) {
+				properties.put(key, newVal);
+				result = updateObject();
+			}
+		} catch (final Exception e) {
+			log.warn("locPutIfUnchanged error : "+e.getMessage());
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * returns agent properties as a mapped collection of JSON nodes
+	 *
+	 * @return the properties
+	 */
+	public Map<String, JsonNode> getProperties() {
+		return properties;
+	}
+	
+	/**
+	 * set all property values from a collection
+	 *
+	 * @param properties the properties
+	 */
+	public void setProperties(final Map<String, JsonNode> properties) {
+		final String agentType = properties.get(KEY_AGENT_TYPE).textValue();
+		properties.clear();
+		this.properties.putAll(properties);
+		this.properties.put(KEY_AGENT_TYPE, JOM.getInstance().valueToTree(agentType));
+		updateObject();
+	}
+	
+	/**
+	 * Finer granularity update command, not working yet
+	 */
+	private synchronized boolean updateField(String field, JsonNode value) {
+		WriteResult result = connection.getCollection(COLLECTION_NAME).
+				update("{id: #}", getAgentId()).
+				with("{$set: {#, #}}", field, value);
+		if (!result.getLastError().ok()) { 
+			log.error("update error : "+result.getError());
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * in its current implementation, the whole object is being updated all at the same time.
+	 * probably will perform better on a finer granularity updates 
+	 * 
+	 */
+	private synchronized boolean updateObject() {
+		 WriteResult result = connection.getCollection(COLLECTION_NAME).save(this);
+		 if (!result.getLastError().ok()) { 
+			 log.error("update error : "+result.getError());
+			 return false;
+		 }
+		 return true;
+	}
+
+}
---
+++ java/eve-core/src/main/java/com/almende/eve/state/mongo/MongoStateFactory.java
@@ -0,0 +1,168 @@
+package com.almende.eve.state.mongo;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.jongo.Find;
+import org.jongo.Jongo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.almende.eve.state.State;
+import com.almende.eve.state.StateFactory;
+import com.mongodb.MongoClient;
+
+/**
+ * An implementation of state factory using MongoDB & Jongo as database connection
+ * 
+ * @author ronny
+ *
+ */
+public class MongoStateFactory implements StateFactory {
+	
+	private static final Logger log = LoggerFactory.getLogger(MongoStateFactory.class);
+	
+	private final Jongo jongo;
+	
+	/**
+	 * default constructor which will connect to default mongodb client
+	 * (localhost:27017) with "eve" as its default database
+	 * 
+	 * @throws UnknownHostException 
+	 */
+	public MongoStateFactory() throws UnknownHostException {
+		this(new MongoClient(), "eve");
+	}
+	
+	/**
+	 * constructor with the URI & database name as its parameter
+	 * 
+	 * @param mongoUriHost
+	 * @param mongoPort
+	 * @param databaseName
+	 * @throws UnknownHostException
+	 */
+	public MongoStateFactory(String mongoUriHost, int mongoPort, String databaseName) throws UnknownHostException {
+		this(new MongoClient(mongoUriHost, mongoPort), databaseName);
+	}
+	
+	/**
+	 * constructor which uses readily available mongo client instance and database name
+	 * @param mongoClient
+	 * @param databaseName
+	 */
+	public MongoStateFactory(MongoClient mongoClient, String databaseName) {
+		this(new Jongo(mongoClient.getDB(databaseName)));
+	}
+	
+	/**
+	 * constructor which uses jongo instantiated elsewhere
+	 * @param jongo
+	 */
+	public MongoStateFactory(Jongo jongo) {
+		this.jongo = jongo;
+	}
+	
+	/**
+	 * returns jongo connection to the underlying mongo database
+	 * @return jongo
+	 */
+	public Jongo getJongo() {
+		return jongo;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.almende.eve.state.StateFactory#get(java.lang.String)
+	 */
+	@Override
+	public State get(String agentId) {
+		MongoState result = null;
+		try {
+			result = jongo.getCollection(MongoState.COLLECTION_NAME).
+							findOne("{_id: #}", agentId).as(MongoState.class);
+			if (result!=null) {
+				result.setConnection(jongo);
+			}
+		} catch (final Exception e) {
+			log.warn("get error:"+e.getMessage());
+		}
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.almende.eve.state.StateFactory#create(java.lang.String)
+	 */
+	@Override
+	public synchronized State create(String agentId) throws IOException {
+		if (exists(agentId)) {
+			throw new IllegalStateException("Cannot create state, "
+					+ "state with id '" + agentId + "' already exists.");
+		}
+		
+		MongoState state = new MongoState(agentId);
+		try {
+			jongo.getCollection(MongoState.COLLECTION_NAME).insert(state);
+		} catch (final Exception e) {
+			log.warn("create error:"+e.getMessage());
+		}
+		state.setConnection(jongo);
+		return state;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.almende.eve.state.StateFactory#delete(java.lang.String)
+	 */
+	@Override
+	public void delete(String agentId) {
+		try {
+			jongo.getCollection(MongoState.COLLECTION_NAME).remove("{_id: #}", agentId);
+		} catch (final Exception e) {
+			log.warn("delete error : "+e.getMessage());
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.almende.eve.state.StateFactory#exists(java.lang.String)
+	 */
+	@Override
+	public boolean exists(String agentId) {
+		MongoState result = jongo.getCollection(MongoState.COLLECTION_NAME).
+								findOne("{_id: #}", agentId).as(MongoState.class);
+		return (result != null);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.almende.eve.state.StateFactory#getAllAgentIds(java.lang.String)
+	 */
+	@Override
+	public Iterator<String> getAllAgentIds() {
+		List<String> agentIDs = new ArrayList<String>();
+		try {
+			Find find = jongo.getCollection(MongoState.COLLECTION_NAME).find().projection("{_id:1}");
+			// :: there's probably a faster way to iterate over id fields
+	        for (Object map : find.as(Object.class)) {
+				String agentId = (String) ((LinkedHashMap) map).get("_id");
+	        	agentIDs.add(agentId);
+	        }
+		} catch (final Exception e) {
+			log.warn("getAllAgentIds error : "+e.getMessage());
+		}
+		return agentIDs.iterator();
+	}
+
+}
+
