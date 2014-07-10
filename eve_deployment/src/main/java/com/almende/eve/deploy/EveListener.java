0910272a31845d93b7fef6e3b1168c2a5ce88b5f
Ludo Stellingwerff <ludo@almende.org>
2014-07-10 13:57:31 +0200

    Make use of the new Boot methods in the EveListener.

--- eve_deployment/src/main/java/com/almende/eve/deploy/EveListener.java
+++ eve_deployment/src/main/java/com/almende/eve/deploy/EveListener.java
@@ -11,15 +11,6 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import com.almende.eve.agent.Agent;
-import com.almende.eve.agent.AgentBuilder;
-import com.almende.eve.agent.AgentConfig;
-import com.almende.eve.capabilities.Config;
-import com.almende.eve.config.YamlReader;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
 /**
  * The listener interface for receiving ServletContext events.
  */
@@ -43,19 +34,8 @@ public class EveListener implements ServletContextListener {
 						+ sc.getRealPath(fullname));
 				return;
 			}
-			final Config config = YamlReader.load(is).expand();
-			
-			// Config is now a Jackson JSON DOM, 'expand()' allows for template resolvement in the configuration.
-			
-			// Now we instantiate two example agents, getting their classpath (and configuration) from the DOM
-			final ArrayNode agents = (ArrayNode) config.get("agents");
-			for (final JsonNode agent : agents) {
-				final AgentConfig agentConfig = new AgentConfig(
-						(ObjectNode) agent);
-				final Agent newAgent = new AgentBuilder().with(agentConfig)
-						.build();
-				LOG.info("Created agent:" + newAgent.getId());
-			}
+
+			Boot.loadAgents(is);
 		}
 		
 	}
