e4e5465a6885079840444596008d60fa65d05b66
Ludo Stellingwerff <ludo@almende.org>
2015-01-06 16:26:26 +0100

    Reintroducing AgentHost for easier migration of old projects.

---
+++ eve_agents/src/main/java/com/almende/eve/agent/AgentHost.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright: Almende B.V. (2014), Rotterdam, The Netherlands
+ * License: The Apache Software License, Version 2.0
+ */
+package com.almende.eve.agent;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.almende.eve.scheduling.SimpleSchedulerConfig;
+import com.almende.eve.state.memory.MemoryStateConfig;
+import com.almende.util.TypeUtil;
+
+/**
+ * The Class AgentHost, this is a backward compatibility class for easier migration of Eve2X implementations to Eve3. 
+ *  
+ */
+@Deprecated
+public class AgentHost {
+	private static final AgentHost	SINGLETON	= new AgentHost();
+	private Map<String, Agent>		agents		= new HashMap<String, Agent>();
+
+	/**
+	 * Gets the single instance of AgentHost.
+	 *
+	 * @return single instance of AgentHost
+	 */
+	public static AgentHost getInstance() {
+		return SINGLETON;
+	}
+
+	/**
+	 * Gets the agent.
+	 *
+	 * @param agentId
+	 *            the agent id
+	 * @return the agent
+	 */
+	public Agent getAgent(final String agentId) {
+		Agent result = null;
+		if (agents.containsKey(agentId)) {
+			result = agents.get(agentId);
+		}
+		return result;
+	}
+
+	/**
+	 * Delete agent.
+	 *
+	 * @param agentId
+	 *            the agent id
+	 */
+	public void deleteAgent(final String agentId) {
+		if (agents.containsKey(agentId)) {
+			agents.remove(agentId);
+		}
+	}
+
+	/**
+	 * Adds the agent.
+	 *
+	 * @param agentId
+	 *            the agent id
+	 * @param agent
+	 *            the agent
+	 */
+	public void addAgent(final String agentId, final Agent agent) {
+		agents.put(agentId, agent);
+	}
+
+	/**
+	 * Creates the agent.
+	 *
+	 * @param agentType
+	 *            the agent type
+	 * @param agentId
+	 *            the agent id
+	 * @return the agent
+	 */
+	public Agent createAgent(String agentType, String agentId) {
+		final AgentConfig config = new AgentConfig();
+		config.setClassName(agentType);
+		config.setId(agentId);
+		config.setScheduler(new SimpleSchedulerConfig());
+		config.setState(new MemoryStateConfig());
+		
+		final Agent agent = new AgentBuilder().with(config).build();
+		
+		addAgent(agentId, agent);
+		return agent;
+	}
+	
+	/**
+	 * Creates the agent.
+	 *
+	 * @param <T>
+	 *            the generic type
+	 * @param agentType
+	 *            the agent type
+	 * @param agentId
+	 *            the agent id
+	 * @return the t
+	 */
+	public <T extends Agent> T createAgent(Class<T> agentType, String agentId) {
+		final AgentConfig config = new AgentConfig();
+		config.setClassName(agentType.getName());
+		config.setId(agentId);
+		config.setScheduler(new SimpleSchedulerConfig());
+		config.setState(new MemoryStateConfig());
+		
+		final T agent = new TypeUtil<T>(){}.inject(new AgentBuilder().with(config).build());
+		
+		addAgent(agentId, agent);
+		return agent;
+	}
+}
