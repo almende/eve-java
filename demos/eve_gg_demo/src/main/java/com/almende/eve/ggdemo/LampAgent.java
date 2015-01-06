a0020ac14bede1243608c0dc7bb58ab6ff467c5c
Ludo Stellingwerff <ludo@almende.org>
2015-01-06 16:34:27 +0100

    Introducing global goal demo

---
+++ demos/eve_gg_demo/src/main/java/com/almende/eve/ggdemo/LampAgent.java
@@ -0,0 +1,28 @@
+package com.almende.eve.ggdemo;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.annotation.ThreadSafe;
+
+import com.almende.eve.agent.AgentInterface;
+import com.almende.eve.protocol.jsonrpc.annotation.Access;
+import com.almende.eve.protocol.jsonrpc.annotation.AccessType;
+import com.almende.eve.protocol.jsonrpc.annotation.Name;
+import com.almende.eve.protocol.jsonrpc.annotation.Sender;
+import com.almende.eve.protocol.jsonrpc.formats.JSONRPCException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+@Access(AccessType.PUBLIC)
+@ThreadSafe
+public interface LampAgent extends AgentInterface {
+	
+	public void create(@Name("neighbours") ArrayList<String> neighbours,
+			@Name("stepSize") Integer stepSize) throws JSONRPCException, IOException;
+	
+	public boolean isOn();
+	public boolean isOnBlock() throws InterruptedException;
+	public void handleGoal(@Name("goal") Goal goal, @Sender String sender) throws 
+			JSONRPCException, JsonProcessingException, IOException;
+	public Iterable<String> getNeighbours();
+}
