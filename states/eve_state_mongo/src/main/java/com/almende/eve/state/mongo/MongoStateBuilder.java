634af801e36839c11e2cb2a29d6073087cdca3f4
Shravan Shetty <sshetty@ask-cs.com>
2015-11-18 19:19:20 +0100

    update: create single instance of MongoClient

--- states/eve_state_mongo/src/main/java/com/almende/eve/state/mongo/MongoStateBuilder.java
+++ states/eve_state_mongo/src/main/java/com/almende/eve/state/mongo/MongoStateBuilder.java
@@ -14,28 +14,34 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
 import org.jongo.Jongo;
 import org.jongo.MongoCollection;
-
+import org.jongo.marshall.jackson.JacksonMapper;
+import org.jongo.marshall.jackson.configuration.MapperModifier;
 import com.almende.eve.capabilities.AbstractCapabilityBuilder;
 import com.almende.eve.state.State;
 import com.almende.eve.state.StateService;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 
 /**
  * The Class MongoStateBuilder.
  */
 public class MongoStateBuilder extends AbstractCapabilityBuilder<MongoState> {
-	private static final Logger						LOG			= Logger.getLogger(MongoStateBuilder.class
-																		.getName());
-	private static Map<String, MongoStateProvider>	instances	= new ConcurrentHashMap<String, MongoStateProvider>();
 
+    private static final Logger LOG = Logger.getLogger(MongoStateBuilder.class.getName());
+    private static Map<String, MongoStateProvider> instances = new ConcurrentHashMap<String, MongoStateProvider>();
+    private static MongoClient client = null;
+    
 	@Override
 	public MongoState build() {
 		final MongoStateProvider provider = getInstanceByParams(getParams());
@@ -47,38 +53,90 @@ public class MongoStateBuilder extends AbstractCapabilityBuilder<MongoState> {
 		}
 	}
 
-	/**
-	 * Gets the instance by params.
-	 * 
-	 * @param params
-	 *            the params
-	 * @return the instance by params
-	 */
-	public MongoStateProvider getInstanceByParams(final ObjectNode params) {
-		final MongoStateConfig config = MongoStateConfig.decorate(params);
-		final String key = config.getKey();
-
-		if (instances.containsKey(key)) {
-			return instances.get(key);
-		} else {
-			synchronized (instances) {
-				if (!instances.containsKey(key)) {
-					try {
-
-						final MongoStateProvider result = new MongoStateProvider(
-								params);
-						if (result != null) {
-							instances.put(key, result);
-						}
-					} catch (final UnknownHostException e) {
-						LOG.log(Level.WARNING,
-								"Couldn't init MongoStateService", e);
-					}
-				}
-				return instances.get(key);
-			}
-		}
-	}
+        /**
+         * Gets the instance by params.
+         * 
+         * @param params
+         *            the params
+         * @return the instance by params
+         */
+        public MongoStateProvider getInstanceByParams(final ObjectNode params) {
+    
+            final MongoStateConfig config = MongoStateConfig.decorate(params);
+            final String key = config.getKey();
+    
+            if (instances.containsKey(key)) {
+                return instances.get(key);
+            }
+            else {
+                synchronized (instances) {
+                    if (!instances.containsKey(key)) {
+                        try {
+                            // initialization of client & jongo
+                            MongoClient client = getClientInstance(config.getHosts(), null);
+                            final MongoStateProvider result = new MongoStateProvider(getJongo(client, 
+                                config.getDatabase()), config);
+                            if (result != null) {
+                                instances.put(key, result);
+                            }
+                        }
+                        catch (final UnknownHostException e) {
+                            LOG.log(Level.WARNING, "Couldn't init MongoStateService", e);
+                        }
+                    }
+                    return instances.get(key);
+                }
+            }
+        }
+        
+        /**
+         * Returns a single instance of the MongoClient
+         * 
+         * @param hosts
+         *            Arraynode of "host" and corresponding "ports"
+         * @return
+         * @throws UnknownHostException
+         */
+        public static MongoClient getClientInstance(final ArrayNode hosts, final List<MongoCredential> credentials)
+            throws UnknownHostException {
+    
+            if (client == null) {
+                final MongoClientOptions options = MongoClientOptions.builder().connectionsPerHost(500)
+                                                                     .threadsAllowedToBlockForConnectionMultiplier(1500)
+                                                                     .build();
+                client = new MongoClient(getHosts(hosts), credentials, options);
+            }
+            return client;
+        }
+        
+        public static Jongo getJongo(final MongoClient client, String dataBaseName) {
+    
+            return new Jongo(client.getDB(dataBaseName), new JacksonMapper.Builder().addModifier(new MapperModifier() {
+    
+                @Override
+                public void modify(ObjectMapper mapper) {
+    
+                    mapper.setSerializationInclusion(JsonInclude.Include.ALWAYS);
+                    mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+                }
+            }).registerModule(new JodaModule()).withView(MongoState.class).build());
+        }
+        
+        /**
+         * Get the list of all hosts in the ArrayNode
+         * 
+         * @param hosts
+         * @return
+         * @throws UnknownHostException
+         */
+        private static List<ServerAddress> getHosts(ArrayNode hosts) throws UnknownHostException {
+    
+            List<ServerAddress> addresses = new ArrayList<ServerAddress>();
+            for (final JsonNode host : hosts) {
+                addresses.add(new ServerAddress(host.get("host").asText(), host.get("port").asInt()));
+            }
+            return addresses;
+        }
 
 	/**
 	 * A service for managing MemoryState objects.
@@ -86,52 +144,36 @@ public class MongoStateBuilder extends AbstractCapabilityBuilder<MongoState> {
 	public class MongoStateProvider implements StateService {
 
 		/* internal attributes */
-		private final Jongo		jongo;
+		private Jongo		jongo;
 		private final String	collectionName;
 
-		/**
-		 * Instantiates a new mongo state service.
-		 * 
-		 * @param params
-		 *            the params
-		 * @throws UnknownHostException
-		 *             the unknown host exception
-		 */
-		public MongoStateProvider(final ObjectNode params)
-				throws UnknownHostException {
-
-			final MongoStateConfig config = MongoStateConfig.decorate(params);
-
-			// initialization of client & jongo
-			final MongoClient client = createClient(config.getHosts());
-			jongo = new Jongo(client.getDB(config.getDatabase()));
-			collectionName = config.getCollection();
-
-			jongo.runCommand("{collMod: '" + collectionName
-					+ "', usePowerOf2Sizes : true }");
-
-		}
-
-		private MongoClient createClient(final ArrayNode hosts) throws UnknownHostException {
-			final MongoClientOptions options = MongoClientOptions.builder()
-					.connectionsPerHost(100)
-					.threadsAllowedToBlockForConnectionMultiplier(1500).build();
-			List<ServerAddress> addresses = new ArrayList<ServerAddress>();
-			for(final JsonNode host : hosts) {
-			    addresses.add( new ServerAddress(host.get("host").asText(), host.get("port").asInt()) );
-			}
-			final MongoClient client = new MongoClient(addresses, options);
-			return client;
-		}
+                /**
+                 * Instantiates a new mongo state service.
+                 * 
+                 * @param client
+                 * 
+                 * @param params
+                 *            the params
+                 * @throws UnknownHostException
+                 *             the unknown host exception
+                 */
+                public MongoStateProvider(final Jongo jongo, final MongoStateConfig config) throws UnknownHostException {
+        
+                    collectionName = config.getCollection();
+                    this.jongo = jongo;
+                }
 
 		/**
 		 * returns jongo collection used by this state factory.
 		 * 
 		 * @return the collection
 		 */
-		public MongoCollection getCollection() {
-			return jongo.getCollection(collectionName);
-		}
+                public MongoCollection getInstance() {
+        
+                    final MongoCollection collection = jongo.getCollection(collectionName);
+                    collection.ensureIndex("{ _id: 1, timestamp:1 }");
+                    return collection;
+                }
 
 		/**
 		 * Gets the.
@@ -140,29 +182,29 @@ public class MongoStateBuilder extends AbstractCapabilityBuilder<MongoState> {
 		 *            the params
 		 * @return the mongo state
 		 */
-		public MongoState get(final ObjectNode params) {
-
-			final MongoStateConfig config = MongoStateConfig.decorate(params);
-			final String id = config.getId();
-
-			MongoState result = null;
-			try {
-				synchronized (this) {
-					result = getCollection().findOne("{_id: #}", id).as(
-							MongoState.class);
-					if (result == null) {
-						result = new MongoState(id, this, params);
-						getCollection().insert(result);
-					} else {
-						result.setService(this);
-					}
-				}
-				result.setCollection(getCollection());
-			} catch (final Exception e) {
-				LOG.log(Level.WARNING, "get error", e);
-			}
-			return result;
-		}
+                public MongoState get(final ObjectNode params) {
+        
+                    final MongoStateConfig config = MongoStateConfig.decorate(params);
+                    final String id = config.getId();
+        
+                    MongoState result = null;
+                    try {
+                        synchronized (this) {
+                            result = getInstance().findOne("{_id: #}", id).as(MongoState.class);
+                            if (result == null) {
+                                result = new MongoState(id, this, params);
+                                getInstance().insert(result);
+                            }
+                            else {
+                                result.setService(this);
+                            }
+                        }
+                    }
+                    catch (final Exception e) {
+                        LOG.log(Level.WARNING, "get error", e);
+                    }
+                    return result;
+                }
 
 		/*
                  * (non-Javadoc)
@@ -183,7 +225,7 @@ public class MongoStateBuilder extends AbstractCapabilityBuilder<MongoState> {
                 public void delete(final State instance, final Boolean instanceOnly) {
                         if(!instanceOnly) {
                             try {
-                                    getCollection().remove("{_id: #}", instance.getId());
+                                    getInstance().remove("{_id: #}", instance.getId());
                             } catch (final Exception e) {
                                     LOG.log(Level.WARNING, "delete error", e);
                             }
@@ -192,7 +234,7 @@ public class MongoStateBuilder extends AbstractCapabilityBuilder<MongoState> {
 
 		@Override
 		public Set<String> getStateIds() {
-			Iterator<MongoState> res = getCollection().find()
+			Iterator<MongoState> res = getInstance().find()
 					.as(MongoState.class).iterator();
 			Set<String> result = new HashSet<String>();
 			while (res.hasNext()) {
